1d01dd4c30ffc01d37889b46f4899f94a394631f
陳鈞 <jim60105@gmail.com>
2020-07-06 04:24:46 +0800

    修改ssh.pas改用secrets處理 修改restore.sh的邏輯，以和backup相同

--- shellScript/backup.sh
+++ shellScript/backup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-date +"%F %T"
-
 scriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Make or clean backup folder
@@ -9,13 +7,19 @@ mkdir -p /backup
 cd /backup
 rm -f *
 
+echo -n "Backup start at $(curl -s http://ipv4.icanhazip.com) " > /backup/log.txt
+echo $(date +"%F %T") >> /backup/log.txt
+
 source ${scriptFolder}/stopContainer.sh
 
 V=$(su - root -c "docker volume ls -f 'label=proxy' -q && docker volume ls -f 'label=nextcloud' -q")
 for i in ${V}
 do
-    echo "Backup ${i}..."
-    su - root -c "docker run -v ${i}:/volume -v $PWD:/backup --rm loomchild/volume-backup backup -v -c gz ${i}"
+    echo "Backup ${i}..." | tee -a /backup/log.txt
+    su - root -c "docker run -v ${i}:/volume -v $PWD:/backup --rm loomchild/volume-backup backup -c gz ${i} | tee -a /backup/log.txt"
 done
+
+echo "" >> /backup/log.txt
+echo "Done at: $(date +"%F %T")" >> /backup/log.txt
 
 source ${scriptFolder}/startContainer.sh
--- shellScript/restore.sh
+++ shellScript/restore.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-
 date +"%F %T"
 
-source restore_noStart.sh
+scriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source startContainer.sh
+# Move to backup folder
+mkdir -p /backup
+cd /backup
+
+source ${scriptFolder}/stopContainer.sh
+
+V=$(su - root -c "docker volume ls -f 'label=proxy' -q && docker volume ls -f 'label=nextcloud' -q")
+for i in ${V}
+do
+    echo "Restore ${i}..."
+    su - root -c "docker run -v ${i}:/volume -v $PWD:/backup --rm loomchild/volume-backup restore -c gz ${i}"
+done
